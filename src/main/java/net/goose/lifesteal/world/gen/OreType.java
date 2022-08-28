ef2ff67efd7de32108b475c4c222700a208d7009
XariZaru <jonsnguyening@gmail.com>
2022-08-28 11:35:54 -0700

    Changelog | 2.8.1
    THE 3 CHANGES BELOW APPLY TO EVERY HEART ORE VARIANT
    - Heart Ore vein size was increased
    - Heart Ore vein amount in each chunk was increased
    - Ore texture was edited a bit
    
    1.18.2/1.19.2 Specific Changes
    - Deepslate Heart Ore is easier to break

--- src/main/java/net/goose/lifesteal/world/gen/OreType.java
+++ src/main/java/net/goose/lifesteal/world/gen/OreType.java
@@ -5,8 +5,8 @@ import net.minecraft.world.level.block.Block;
 import net.minecraftforge.common.util.Lazy;
 
 public enum OreType {
-    HEART_ORE(Lazy.of(ModBlocks.HEART_ORE), 10, 12, 50, 10),
-    NETHERRACK_HEART_ORE(Lazy.of(ModBlocks.NETHERRACK_HEART_ORE), 12, 12, 50, 12);
+    HEART_ORE(Lazy.of(ModBlocks.HEART_ORE), 12, 10, 60, 12),
+    NETHERRACK_HEART_ORE(Lazy.of(ModBlocks.NETHERRACK_HEART_ORE), 14, 10, 80, 14);
 
 
     private final Lazy<Block> Block;
